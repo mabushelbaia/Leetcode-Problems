014f2956430d168dc127db2afabd5be98ba11907
mabushelbaia <mabushelbaia@gmail.com>
2022-06-28 06:51:53 +0300

    Create Script

---
+++ new.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$#" -lt 2 ]; then echo "invalid args"; exit 0; fi
+if ls -la | grep ./"$1" > /dev/null; then
+	touch ./$1/main.$2
+else
+	mkdir ./$1
+	touch ./$1/main.$2
+fi
+code ./$1
