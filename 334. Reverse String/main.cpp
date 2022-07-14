94708a6e9b41de2f39941d9532da0668dc345a3a
mabushelbaia <mabushelbaia@gmail.com>
2022-07-14 17:58:00 +0300

    > 334. Reverse String

---
+++ 334. Reverse String/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start = 0, end = s.size() - 1;
+        while (start < end){
+            int temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+};
